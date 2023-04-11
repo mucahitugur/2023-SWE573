03e2a14f11d3eb02bc7867891d46f3c034d4c92c
mucahitugur <54595971+mucahitugur@users.noreply.github.com>
2023-04-11 15:32:44 +0300

    Change profile photo is added

--- accounts/forms.py
+++ accounts/forms.py
@@ -11,13 +11,23 @@ class CustomUserCreationForm(UserCreationForm):
         model = User
         fields = UserCreationForm.Meta.fields + ('email',)
 
-    def save(self, commit=True):
+    def save(self, commit=True, update_profile_picture=False):
         user = super().save(commit=False)
         user.email = self.cleaned_data["email"]
         if commit:
             user.save()
-            profile = UserProfile(user=user)
-            if 'profile_picture' in self.cleaned_data and self.cleaned_data['profile_picture']:
-                profile.profile_picture = self.cleaned_data['profile_picture']
-            profile.save()
+            if not update_profile_picture:
+                profile = UserProfile(user=user)
+                if 'profile_picture' in self.cleaned_data and self.cleaned_data['profile_picture']:
+                    profile.profile_picture = self.cleaned_data['profile_picture']
+                profile.save()
+            else:
+                user.userprofile.profile_picture = self.cleaned_data['profile_picture']
+                user.userprofile.save()
         return user
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['profile_picture']
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,24 +1,23 @@
-from django.shortcuts import render
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django.views import generic
-
-class SignUpView (generic.CreateView):
-    form_class=UserCreationForm
-    success_url=reverse_lazy('login')
-    template_name='signup.html'
-
-
-
-
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views import generic
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, UpdateProfileForm
 
 class SignUpView(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+@login_required
+def update_profile(request):
+    if request.method == 'POST':
+        form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.userprofile)
+        if form.is_valid():
+            form.save()
+            return redirect('my_profile')
+    else:
+        form = UpdateProfileForm(instance=request.user.userprofile)
+
+    context = {'form': form}
+    return render(request, 'update_profile.html', context)
